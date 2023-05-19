4b39ccacfc5a88af66e6318a8ae1f39f0c7ee3f7
Collins Kasyoki <collinskasyoki@gmail.com>
2023-05-19 09:31:56 +0300

    Add main script file

---
+++ screen.py
@@ -0,0 +1,16 @@
+import os
+
+SCRIPT_PATH = os.path.dirname(__file__)
+SCREENSHOT_PATH = os.path.join(SCRIPT_PATH, "screenshots")
+
+
+def take_screenshot():
+    pass
+
+
+def compare_screenshots():
+    pass
+
+
+def send_alert():
+    pass
